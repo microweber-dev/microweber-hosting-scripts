d80f52fe2cb5f65a944a174e0c00be08ec596efb
Peter Ivanov <peter@microweber.com>
2015-03-24 20:13:09 +0200

    options array added

--- virtualmin/install.php
+++ virtualmin/install.php
@@ -24,10 +24,11 @@ if (isset($config["VIRTUALSERVER_ACTION"])) {
         $opts['database_name'] = $config["VIRTUALSERVER_DB_MYSQL"];
         $opts['database_user'] = $config["VIRTUALSERVER_MYSQL_USER"];
         $opts['database_password'] = $config["VIRTUALSERVER_PASS"];
-		
-		// $opts['options'] = array(
-        //     array('option_key'=>'website_title','option_value'=>'My New Website','option_group'=>'website')
-        // );
+
+        //  $opts['options'] = array(
+        //      array('option_key' => 'website_title', 'option_value' => 'Boxed Web', 'option_group' => 'website'),
+        //      array('option_key' => 'items_per_page', 'option_value' => '30', 'option_group' => 'website')
+        //  );
 		
 		
         require __DIR__ . "/run.php";
