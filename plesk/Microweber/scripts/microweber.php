bc0fc6b12365302fa70226641a17784a5746930c
Peter Ivanov <peter@microweber.com>
2015-07-24 18:07:37 +0300

    plesk

--- plesk/Microweber/scripts/microweber.php
+++ plesk/Microweber/scripts/microweber.php
@@ -2,7 +2,7 @@
 
 
 if (!function_exists('get_table_prefix')) {
-    function get_table_prefix($db_modify_hash, $psa_modify_hash, $db_ids)
+    function get_table_prefix($db_modify_hash, $psa_modify_hash, $db_ids=array())
     {
         $config_content = read_file($psa_modify_hash['@@ROOT_DIR@@'] . '/wp-config.php');
         if (preg_match('#^\$table_prefix *= *[\'\"]([^\'\"]*)[\'\"]#m', $config_content, $mres)) {
--- plesk/install.php
+++ plesk/install.php
@@ -15,11 +15,11 @@ function string_between($string, $start, $end)
 if (!isset($_SERVER['NEW_DOMAIN_NAME'])) {
     return;
 }
-
+$cmd_out = '';
 $domain = $_SERVER['NEW_DOMAIN_NAME'];
 
 $db_name = str_replace(array('.', '-'), '', $domain);
-$db_name = "mw_" . substr($db_name, 0, 8);
+$db_name = "mw_" . substr($db_name, 0, 8). rand();
 $db_user = "mw_" . uniqid();
 $db_pass = md5(uniqid() . uniqid() . rand());
 
@@ -28,12 +28,9 @@ $mysql_cmd = "/usr/local/psa/bin/database --create {$db_name} -domain {$domain} 
 $mysql_make_user = "/usr/local/psa/bin/database -u $db_name -add_user {$db_user} -passwd {$db_pass}";
 
 
-$install_aps = "aps --install \"Microweber\" -package-id 13 -domain example.com -ssl false -url-prefix blog -db-name WordPress -db-user BlogAdmin -passwd P4$$w0rd";
-
-
 $get_aps = "/usr/local/psa/bin/aps -gp ";
 $output = '';
-exec("$get_aps 2>&1", $output, $return_var);
+//$output = exec("$get_aps 2>&1", $output, $return_var);
 
 $outputs = explode('================================', $output);
 $apps = array();
@@ -46,65 +43,83 @@ if (!empty($outputs)) {
 }
 
 
-$install_aps = "/usr/local/psa/bin/aps --install \"/usr/share/microweber-hosting-scripts/plesk/Microweber/APP-META.xml\" -package-id 8 -domain {$domain} ";
+//$install_aps = "/usr/local/psa/bin/aps --install -\"/usr/share/microweber-hosting-scripts/plesk/Microweber/APP-META.xml\" -package-id 8 -url-prefix / -domain {$domain} ";
+//$install_aps = "php -f /usr/share/microweber-hosting-scripts/plesk/Microweber/APP-META.php |  /usr/local/psa/bin/aps --install - -package-id 8 -url-prefix / -domain {$domain} ";
 
+
+//$install_aps = "php -f /usr/share/microweber-hosting-scripts/plesk/Microweber/APP-META.php|/usr/local/psa/bin/aps --install - -package-id 8 -url-prefix / -domain {$domain} ";
+//$install_aps .= "-db-server localhost:3306 -db-name {$db_name} -prefix myblog_ -db-user {$db_user} -passwd {$db_pass}";
+//
+//$install_aps = "/usr/local/psa/bin/aps --install - -package-id 8 -url-prefix / -domain {$domain} ";
+//$install_aps .= "-db-server localhost:3306 -db-name {$db_name} -prefix myblog_ -db-user {$db_user} -passwd {$db_pass}";
+
+
+include_once("/usr/share/microweber-hosting-scripts/plesk/Microweber/APP-META.php");
+
+
+if (isset($xml_file)) {
+    $install_aps = "/usr/local/psa/bin/aps --install {$xml_file} -package-id 8 -url-prefix / -domain {$domain} -db-server localhost:3306 -db-name {$db_name} -prefix mw_ -db-user {$db_user} -passwd {$db_pass}";
+    $install_aps = "sudo /usr/local/psa/bin/aps --install {$xml_file} -package-id 8 -url-prefix / -domain {$domain} -db-server localhost:3306 -db-name {$db_name} -prefix mw_ -db-user {$db_user} -passwd {$db_pass}";
+    $install_aps = "runuser -l  root -c '/usr/local/psa/bin/aps --install {$xml_file} -package-id 8 -url-prefix / -domain {$domain} -db-server localhost:3306 -db-name {$db_name} -prefix mw_ -db-user {$db_user} -passwd {$db_pass}'";
+    $install_aps = "runuser -l  root -c '/usr/local/psa/bin/aps --install {$xml_file} -package-id 8 -url-prefix / -domain {$domain} -db-server localhost:3306 -db-name {$db_name} -prefix mw_ -db-user {$db_user} -passwd {$db_pass}'";
+
+
+
+
+
+
+    $install_aps = "/usr/local/psa/bin/aps --install {$xml_file} -package-id 8 -url-prefix / -domain {$domain} -db-server localhost:3306 -db-name {$db_name} -prefix mw_ -db-user {$db_user} -passwd {$db_pass} > /dev/null &";
+$install_aps_bash = "#!/bin/sh
+echo \"Hi, I'm sleeping for 5 seconds...\"
+sleep 5
+
+/usr/local/psa/bin/aps --install {$xml_file} -package-id 8 -url-prefix / -domain {$domain} -db-server localhost:3306 -db-name {$db_name} -prefix mw_ -db-user {$db_user} -passwd {$db_pass}
+
+echo \"all Done.\"
+";
+
+    file_put_contents('/root/mw_install_for_user.sh', $install_aps_bash);
+    shell_exec("chmod +x /root/mw_install_for_user.sh");
+    shell_exec("/root/mw_install_for_user.sh");
+
+
+
+
+    //$install_aps = "/usr/local/psa/bin/aps --install - -package-id 8 -url-prefix / -domain {$domain} ";
+    //$install_aps .= "-db-server localhost:3306 -db-name {$db_name} -prefix mw_ -db-user {$db_user} -passwd {$db_pass}";
+    //$install_aps .= " < $xml_file";
+} else {
+    return;
+}
+//$cmd_out = shell_exec($mysql_cmd);
+//$cmd_out .= shell_exec($mysql_make_user);
+//$cmd_out .= shell_exec($install_aps);
+//$nohup = "nohup $install_aps &";
+//exec($nohup);
+//shell_exec($install_aps);
 
 $to = 'boksiora@gmail.com';
 $subject = 'plesk ' . $domain;
 $message = json_encode($_SERVER);
+$message .= "\n_SERVER======================\n";
+$message .= json_encode($cmd_out);
+$message .= "\ncmd_out======================\n";
+$message .= json_encode($outputs);
 $message .= "\n======================\n";
-$message .= json_encode($_REQUEST);
-$message .= "\n======================\n";
-$message .= json_encode($argv);
-$message .= "\n======================\n";
-$message .= ($output);
+$message .= json_encode($return_var);
 $message .= "\n======================\n";
 $message .= json_encode($apps);
 $message .= "\n======================\n";
 $message .= ($install_aps);
+$message .= "\n======================\n";
+$message .= ($install_aps_bash);
 
 file_put_contents('/usr/share/microweber-hosting-scripts/plesk/debug.txt', $message);
 mail($to, $subject, $message);
 
-exec($mysql_cmd);
-exec($mysql_make_user);
-exec($install_aps);
-//
-//$config = $_SERVER;
-//
-//if (isset($config["VIRTUALSERVER_ACTION"])) {
-//    if (($config["VIRTUALSERVER_ACTION"]) == 'CREATE_DOMAIN') {
-//
-//        $opts = array();
-//
-//        // folder for the microweber source
-//        // $opts['source_folder'] = '/usr/share/microweber-latest/';
-//
-//        // debug email
-//        // $opts['debug_email'] = 'boksiora@gmail.com';
-//        // $opts['debug_email_subject'] = 'New site';
-//
-//
-//        $opts['user'] = $config["VIRTUALSERVER_USER"];
-//        $opts['pass'] = $config["VIRTUALSERVER_PASS"];
-//        $opts['email'] = $config["VIRTUALSERVER_EMAILTO"];
-//
-//        if (isset($config["VIRTUALSERVER_PUBLIC_HTML_PATH"])) {
-//            $opts['public_html_folder'] = $config ["VIRTUALSERVER_PUBLIC_HTML_PATH"];
-//        }
-//
-//        $opts['default_template'] = 'liteness';
-//        $opts['database_name'] = $config["VIRTUALSERVER_DB_MYSQL"];
-//        $opts['database_user'] = $config["VIRTUALSERVER_MYSQL_USER"];
-//        $opts['database_password'] = $config["VIRTUALSERVER_PASS"];
-//
-//        //  $opts['options'] = array(
-//        //      array('option_key' => 'website_title', 'option_value' => 'My Web', 'option_group' => 'website'),
-//        //      array('option_key' => 'items_per_page', 'option_value' => '30', 'option_group' => 'website')
-//        //  );
-//
-//
-//        require __DIR__ . "/../common/run.php";
-//    }
-//}
+
+if (isset($xml_file)) {
+//@unlink($xml_file);
+}
+
 
